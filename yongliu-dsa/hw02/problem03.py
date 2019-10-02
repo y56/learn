d41ed757ee85a41af1f80a1b77d8ae22048450c0
y56 <york13468@gmail.com>
2019-10-01 20:37:56 -0400

    Fix out-of-index bug and fix fail-to-follow--python-indexing bug

--- yongliu-dsa/hw02/problem03.py
+++ yongliu-dsa/hw02/problem03.py
@@ -7,25 +7,32 @@ Created on Tue Oct  1 17:09:52 2019
 """
 
 def maxHeapify(A, i):
-    leftChild = 2 * i
-#    leftChildValue = A[2 * i]
-    rightChild = 2 * i + 1
-#    rightChildValue = A[2 * i + 1]
-    if i <= len(A) and A[leftChild] > A[i]:
-        largestSoFar = leftChild
+    
+    leftChild = 2 * i + 1
+    rightChild = 2 * i + 2
+    print("heapify", "me", i, "left", leftChild, "right", rightChild)
+    
+    if leftChild < len(A):  # index may go out of range!
+        if A[leftChild] > A[i]:
+            largestSoFar = leftChild
     else:
         largestSoFar = i
-    if rightChild <= len(A) and A[rightChild] > A[largestSoFar]:
-        largestSoFar = rightChild
+        
+    if rightChild < len(A):  # index may go out of range!
+        if A[rightChild] > A[largestSoFar]: 
+            largestSoFar = rightChild
+            
     if largestSoFar != i:
         A[i], A[largestSoFar] = A[largestSoFar], A[i]  # put the largest at i
         maxHeapify(A, largestSoFar)  # Go check the following subtree
 
 def buildMaxHeap(A):
-    for i in range(len(A) // 2, 0, -1):
+    for i in range(len(A) // 2 - 1, -1, -1):
+        print(i, "build")
         maxHeapify(A, i)
         
 
 
 A = [1, 7, 9, 3, 100, 13, 12, 5, 14]
 buildMaxHeap(A)
+print(A)
