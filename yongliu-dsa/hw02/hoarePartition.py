646a9424084bbee6924e819288218937399db6f1
y56 <york13468@gmail.com>
2019-10-02 23:01:27 -0400

    Update

---
+++ yongliu-dsa/hw02/hoarePartition.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  2 19:07:55 2019
+
+Hoare Partion
+
+@author: y56
+"""
+
+def hoarePartion(A, low , high):
+    pivot = A[low]
+    i = low - 1
+    j = high + 1
+    while i <= j:
+        while :
+            
+        
+        while :
+        
+        A[i], A[j] = A[j], A[i]
+    
