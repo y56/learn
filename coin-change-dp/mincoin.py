310d3a7c02ef56abc838ff8b8956932f3e7eaabf
Eugene Wang <36190560+y56@users.noreply.github.com>
2019-12-05 23:10:19 -0500

    Create mincoin.py

---
+++ coin-change-dp/mincoin.py
@@ -0,0 +1,43 @@
+# A Dynamic Programming based Python3 program to  
+# find minimum of coins to make a given change V 
+  
+# m is size of coins array (number of  
+# different coins) 
+def minCoins(coins, m, V): 
+
+    INF = float('inf')
+      
+    # table[i] will be storing the minimum  
+    # number of coins required for i value.  
+    # So table[V] will have result 
+    table = [0 for i in range(V + 1)] 
+  
+    # Base case (If given value V is 0) 
+    table[0] = 0
+  
+    # Initialize all table values as Infinite 
+    for i in range(1, V + 1): 
+        table[i] = INF 
+  
+    # Compute minimum coins required  
+    # for all values from 1 to V 
+    for i in range(1, V + 1): 
+          
+        # Go through all coins smaller than i 
+        for j in range(m): 
+            if (coins[j] <= i): 
+                sub_res = table[i - coins[j]] 
+                if (sub_res < INF and 
+                    sub_res + 1 < table[i]): 
+                    table[i] = sub_res + 1
+    return table[V] 
+
+
+# Driver Code 
+if __name__ == "__main__": 
+  
+    coins = [9, 6, 5, 1] 
+    m = len(coins) 
+    V = 11
+    print("Minimum coins required is ",  
+                 minCoins(coins, m, V)) 
