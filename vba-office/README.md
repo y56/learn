ea2391bf903faac595bb7df4329fca1fd9d500d1
y56 <york13468@gmail.com>
2021-08-28 22:09:28 -0400

    feat: Show names of WORD bookmarks in-place

---
+++ vba-office/README.md
@@ -0,0 +1,5 @@
+# Visual Basic Tools for M$ office
+
+## show-bkmk.vb
+
+Print names of WORD bookmarks in-place.
---
+++ vba-office/show-bkmk.vb
@@ -0,0 +1,18 @@
+Sub ShowBkMk()
+    Dim Bmk() As String
+    Dim x As Integer, J As Integer
+ 
+    x = ActiveDocument.Bookmarks.Count
+    ReDim Bmk(x)
+
+    For J = 1 To x
+        Bmk(J) = ActiveDocument.Bookmarks(J).Name
+    
+        Set r = ActiveDocument.Bookmarks(J).Range
+        With r
+            r.HighlightColorIndex = wdBrightGreen
+            .InsertAfter Text:=ActiveDocument.Bookmarks(J).Name
+            '.InsertParagraphAfter
+        End With
+    Next J
+End Sub
