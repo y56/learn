f0838d827c612bf3ebf2cad53de8bff956208e11
Eugene Wang <36190560+y56@users.noreply.github.com>
2021-10-07 11:39:02 -0400

    Create check-by-wo-no.js

---
+++ csi-webpage-tool/check-by-wo-no.js
@@ -0,0 +1,14 @@
+// var target_wo_list =  ['303088'];
+var target_wo_list = result;
+for (const target_wo of target_wo_list){
+    $("span[wonumber=" + target_wo + "]", frames['main'].document)
+    .each(
+        function(){
+			console.log('CW');
+			if (  $(this).closest('tr').find(':checkbox').attr("checked")  === undefined) {
+				console.log('found an unchecked');
+			};
+            $(this).closest('tr').find(':checkbox').attr("checked", 'checked');
+        }
+    )
+}
