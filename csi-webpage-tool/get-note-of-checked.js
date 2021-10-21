2e2218bb48a230dfda198186f734e4f14235f940
Eugene Wang <36190560+y56@users.noreply.github.com>
2021-10-21 11:44:37 -0400

    Update get-note-of-checked.js

--- csi-webpage-tool/get-note-of-checked.js
+++ csi-webpage-tool/get-note-of-checked.js
@@ -4,4 +4,5 @@ for (i = 0; i < L; i++) {
   li.push($("input[type=checkbox][name=chk][checked=checked]", frames['main'].document)[i].closest('tr')['cells'][6]['innerHTML']);
 }
 uniq = [...new Set(li)];
+uniq.sort();
 console.log(uniq);
